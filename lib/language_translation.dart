de7cee795303461e91924231c3e59e4f215fa222
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-20 22:41:37 +0530

    Language Translation

--- lib/language_translation.dart
+++ lib/language_translation.dart
@@ -64,7 +64,29 @@ TextEditingController languageController = TextEditingController();
 
                     SizedBox(width: 50.0,),
                     Icon(Icons.arrow_right_alt_outlined, color: Colors.white, size: 40.0,),
-                    SizedBox(width: 40.0,)
+                    SizedBox(width: 40.0,),
+
+                    DropdownButton(
+                    focusColor: Colors.white,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                    hint: Text(originLanguage, style: TextStyle(color: Colors.white),),
+                    icon: Icon(Icons.keyboard_arrow_down_outlined),
+                    dropdownColor: Colors.white,
+                    items: languages.map((String dropDownStringItem)
+                    {
+                      return DropdownMenuItem(
+                        child: Text(dropDownStringItem),
+                        value: dropDownStringItem,
+                      );
+                    }).toList(),
+                    onChanged: (String? value)
+                    {
+                      setState(() {
+                        originLanguage = value!;
+                      });
+                    }
+                    ),
                 ],
               )
             ],
