001f7d9952358d969f5dcf764421566635ad43a8
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-20 22:29:44 +0530

    LanguageTranslation

--- lib/language_translation.dart
+++ lib/language_translation.dart
@@ -15,6 +15,30 @@ class _LanguageTranslationState extends State<LanguageTranslation> {
       appBar: AppBar(
         title: Text("Language Translator",), centerTitle: true,
         backgroundColor: Color(0xff10223d),
+        elevation: 0,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  DropdownButton(
+                    focusColor: Colors.white,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                    items: items, 
+                    onChanged: onChanged
+                    )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
